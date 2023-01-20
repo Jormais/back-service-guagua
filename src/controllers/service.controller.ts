c0a624f482c779a8199b3ed1336e1f43c99aaa1a
unknown <jonaybrito15@gmail.com>
2023-01-20 20:33:48 +0100

    añadida libreria y funcion basica para su uso

--- src/controllers/service.controller.ts
+++ src/controllers/service.controller.ts
@@ -1,8 +1,28 @@
-import { NextFunction, Request, Response } from 'express';
+//import { NextFunction, Request, Response } from 'express';
+const fs = require('fs');
+const pdf = require('pdf-parse');
+ 
 
 class ServiceController {
-    gerService(req: Request, res: Response) {
-        res.status(200).send('corrtecto');
+    gerService() {
+        
+        let dataBuffer = fs.readFileSync('../pdf/chivata_2429.pdf');
+        pdf(dataBuffer).then(function(data) {
+            // number of pages
+            console.log(data.numpages);
+            // number of rendered pages
+            console.log(data.numrender);
+         // PDF info
+            console.log(data.info);
+            // PDF metadata
+            console.log(data.metadata); 
+            // PDF.js version
+            // check https://mozilla.github.io/pdf.js/getting_started/
+            console.log(data.version);
+            // PDF text
+            console.log(data.text); 
+        
+});
     }
 }
 
